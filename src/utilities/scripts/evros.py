b05f6a6dea1c9327ee73b1ad02c4d03514ab15f8
Timothy Wu <thsuanwu@stanford.edu>
2022-11-13 03:43:36 -0800

    Update evros.py

--- src/utilities/scripts/evros.py
+++ src/utilities/scripts/evros.py
@@ -150,7 +150,7 @@ def main():
         (
             #"PATH=/opt/conda/bin:/opt/cellranger-7.0.1:${PATH}",
             #"echo $PATH",
-            "source activate utilities-env",
+            "conda activate utilities-env",
             "git clone {}".format(REPO_ADDRESS),
             "cd utilities",
             "python setup.py install",
